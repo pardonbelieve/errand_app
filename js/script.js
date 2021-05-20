a431c930ef27839f3c484f7542809408caf5a0a7
Destiny Brotobor <destinybravos@gmail.com>
2021-05-20 19:17:00 +0100

    Updated the mobile menu and banner style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
+
+let navbar = document.getElementById('navbar');
+let overlay = document.getElementById('overlay');
+
 document.getElementById('nav-toggler').addEventListener('click', function () {
-    let navbar = document.getElementById('navbar');
     if(navbar.classList.contains('open')){
         navbar.classList.remove('open');
+        overlay.style.opacity = 0;
+        setTimeout(() => {
+            overlay.style.display = 'none';
+        }, 500);
     }else{
         navbar.classList.add('open');
+        overlay.style.opacity = 1;
+        overlay.style.display = 'block';
+    }  
+})
+
+overlay.addEventListener('click', function(){
+    if(navbar.classList.contains('open')){
+        navbar.classList.remove('open');
+        overlay.style.opacity = 0;
+        setTimeout(() => {
+            overlay.style.display = 'none';
+        }, 500);
+    }else{
+        navbar.classList.add('open');
+        overlay.style.opacity = 1;
+        overlay.style.display = 'block';
     } 
 })
